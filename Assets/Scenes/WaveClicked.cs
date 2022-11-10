0f27311a4e5d5cb82ed326fec786b045484dc27b
Adam Žák <adam.zak@softpoint.sk>
2022-11-11 00:12:53 +0100

    Add changing texts and hiding button (main)

--- Assets/Scenes/WaveClicked.cs
+++ Assets/Scenes/WaveClicked.cs
@@ -2,25 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using static radioBehavior;
+using TMPro;
 
 public class WaveClicked : MonoBehaviour
 {
     private GameObject radioScreen;
+    private TMP_Text subtitles;
+    private float nextActionTime = 0.0f;
+    public float period = 0.3f;
     // Start is called before the first frame update
     void Start()
     {
-        radioScreen = GameObject.Find("radioScreen");
+        radioScreen = GameObject.Find("SpeachToText");
+        subtitles = GameObject.Find("Subtitles").GetComponent<TextMeshProUGUI>();
         //radioScreen.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-    
+        if (Time.time > nextActionTime ) {
+            nextActionTime += period;
+            // execute block of code here
+            subtitles.text = "Time je: " + nextActionTime;
+        }
+        
     }
 
     public void StartVoice(){
         Debug.Log("CLickol si na wave button :)");
+        //subtitles.text = "text";
+        subtitles.enabled = !subtitles.enabled;
     }
 
     public void CloseCanvas(){
