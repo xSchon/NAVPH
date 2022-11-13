63e5a9393197180bbeef303828d06dde35e9890a
Martin Schön <martin.schon@softpoint.sk>
2022-11-13 15:42:05 +0100

    Add changes to map functionality

--- Assets/Scripts/radioBehavior.cs
+++ Assets/Scripts/radioBehavior.cs
@@ -5,24 +5,32 @@ using UnityEngine;
 public class radioBehavior : MonoBehaviour
 {
     private GameObject radioScreen;
+    private GameObject mapScreen;
     private string radioObjectName = "Radio";
+    private string mapObjectName = "Map";
 
     // Start is called before the first frame update
     void Start()
     {
-        radioScreen = GameObject.Find("radioScreen");
-        radioScreen.SetActive(false);
+      radioScreen = GameObject.Find("radioScreen");
+      radioScreen.SetActive(false);
+      mapScreen = GameObject.Find("mapScreen");
+      mapScreen.SetActive(false);
     }
 
     // Update is called once per frame
  void Update(){
-   if (Input.GetMouseButtonDown(0) && !radioScreen.activeSelf){ // if left button pressed AND gui disabled
+   if (Input.GetMouseButtonDown(0) && !radioScreen.activeSelf && !mapScreen.activeSelf){ // if left button pressed AND gui disabled
+     Debug.Log("xdd");
      Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
      RaycastHit hit;
 
      if (Physics.Raycast(ray, out hit)){
         if(hit.collider.gameObject.name == radioObjectName){ // if clicked on the radio 
             radioScreen.SetActive(true);
+        }
+        else if(hit.collider.gameObject.name == mapObjectName){
+            mapScreen.SetActive(true);
         }
         else{
             Debug.Log(hit.collider.gameObject.name);
