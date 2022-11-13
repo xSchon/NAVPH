9690cdbfcfe2cc5faf4f337d43e2b9dc340a26ed
Martin Schön <martin.schon@softpoint.sk>
2022-11-14 00:43:59 +0100

    Add moving army

--- Assets/Scripts/moveArmy.cs
+++ Assets/Scripts/moveArmy.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class moveArmy : MonoBehaviour
 {
     private GameObject sectors;
+    private TMP_Text subtitles;
     // Start is called before the first frame update
     void Start()
     {
-        sectors = GameObject.Find("selectedSectors").GetComponent<TextMeshProUGUI>();
+        sectors = GameObject.Find("selectedSectors");
+        sectors.GetComponent<sectorsDeffence>().changeText();
+
+        //Debug.Log(sectors);
+        //sectors.GetComponent<sectorsDeffence>().changeText();
     }
 
     // Update is called once per frame
