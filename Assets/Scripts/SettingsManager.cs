8b206f6e918539f06e1713fde00f57fa1905a3cc
Lucia Rapánová <45311429+luciarap@users.noreply.github.com>
2022-12-13 22:53:51 +0100

    updated menu, added mute/unmute for sound in the settings

---
+++ Assets/Scripts/SettingsManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsManager : MonoBehaviour
+{
+    [SerializeField] private TMPro.TextMeshProUGUI soundTextMesh;
+
+    void Start()
+    {
+        if (AudioListener.volume == 0)
+        {
+            soundTextMesh.text = "OFF";
+        }
+        else
+        {
+            soundTextMesh.text = "ON";
+        }
+
+    }
+
+    public void MuteAllSounds()
+    {
+        if (soundTextMesh.text == "ON")
+        {
+            AudioListener.volume = 0;
+            soundTextMesh.text = "OFF";
+            Debug.Log("Mute");
+        }
+        else
+        {
+            AudioListener.volume = 1;
+            soundTextMesh.text = "ON";
+            Debug.Log("Unmute");
+        }
+    }
+}
