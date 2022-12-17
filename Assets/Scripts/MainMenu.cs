802497528669bbabe2cb3b753fca6ca512fa1fc4
Martin Schön <martin.schon@softpoint.sk>
2022-12-17 12:28:34 +0100

    Finalize reading files (main)

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,19 +4,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.IO;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 public class MainMenu : MonoBehaviour
 {
-    string[] files = null;
     string folderPath;
     public GameObject dialogueWindow;
     public GameObject continueButton;
 
     void Start()
     {
-        folderPath = Application.persistentDataPath;
-        files = Directory.GetFiles(folderPath);
-        if (files.Length == 0)
+        DirectoryInfo directory = new DirectoryInfo(Application.persistentDataPath);
+        IEnumerable<FileInfo> files = directory.GetFiles();
+        string[] namesSkip = {"prefs", "Player.log", "Player-prev.log"};    
+        files = files.OrderByDescending(f => f.LastWriteTime).Where(f => !namesSkip.Any(f.Name.Contains));
+
+        if (!files.Any())
         {
             continueButton.SetActive(false);
         }
@@ -40,16 +45,19 @@ public class MainMenu : MonoBehaviour
 
     public void NewGameConfirmed()
     {
-        if (files.Length > 0)
+        DirectoryInfo directory = new DirectoryInfo(Application.persistentDataPath);
+        IEnumerable<FileInfo> files = directory.GetFiles();
+        string[] namesSkip = {"prefs", "Player.log", "Player-prev.log"};    
+        files = files.OrderByDescending(f => f.LastWriteTime).Where(f => !namesSkip.Any(f.Name.Contains));
+
+        if (files.Any())
         {
             // 1. delete save file
             try
             {
-                string folderPath = Application.persistentDataPath;
-                IEnumerable<string> files = Directory.EnumerateFiles(folderPath);
-                foreach (string file in files)
+                foreach (FileInfo file in files)
                 {
-                    File.Delete(file);
+                    file.Delete();
                 }
                 // 2. load new game 
                 SceneManager.LoadScene("Video");
